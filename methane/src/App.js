f1d071665d52c13fd4dfa5bf887146e00af9086c
Aloknath-sah <kumaraditya6396@gmail.com>
2021-01-01 21:10:06 +0530

    startfundraiser page created

--- methane/src/App.js
+++ methane/src/App.js
@@ -4,12 +4,16 @@ import { Routes } from './Routes/Routes';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 import CardDetails from './Components/CardDetails/CardDetails'
+import StartFundraiser from "./Components/StartFundraiser/StartFundraiser";
+import NewFundRaiserPage from "./Components/StartFundraiser/NewFundRaiserPage";
 
 function App() {
   return (
     <div className="App">
       {/* <Routes/> */}
-      <CardDetails/>
+      {/* <CardDetails/> */}
+      {/* <StartFundraiser/> */}
+      <NewFundRaiserPage/>
     </div>
   );
 }
---
+++ methane/src/Components/StartFundraiser/NewFundRaiserPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewFundRaiserPage = () => {
+    return (
+        <div>
+        
+        </div>
+    );
+}
+
+export default NewFundRaiserPage;
